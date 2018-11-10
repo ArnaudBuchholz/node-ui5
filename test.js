20e35774bec1971d68c24263f93978390f6d993f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-10 17:01:24 -0500

    Testing with a real OData service

---
+++ test.js
@@ -0,0 +1,18 @@
+require('./index').then(sap => {
+  sap.ui.require([
+    'sap/ui/model/odata/v2/ODataModel'
+  ], function (ODataModel) {
+    console.log('Creating ODataModel...')
+    const model = new ODataModel({
+        serviceUrl: 'https://services.odata.org/V2/OData/OData.svc'
+    })
+    model.metadataLoaded().then(() => {
+      console.log('Loading products...')
+      model.read('/Products', {
+        success: data => {
+          console.log(data)
+        }
+      })
+    })
+  })
+})
