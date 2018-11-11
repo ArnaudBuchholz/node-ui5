13e16ae3bfcfc765bdee030e762d88298191877a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-11 12:07:05 -0500

    Removes type parameter (unused)

--- read.js
+++ read.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const openui5BaseDir = path.join(__dirname, 'node_modules/@openui5/sap.ui.core/dist/resources')
 
-module.exports = (type, url) => {
+module.exports = url => {
   try {
     const match = /(?:\/|^)resources\/(.*)$/.exec(url) // Expect local resources to be under resources
     if (!match) {
