8d88eb2cc2d77591394dfc2d383b7d3f61fde536
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-13 12:35:01 -0500

    Finalizing test method

--- test.js
+++ test.js
@@ -1,18 +1,19 @@
 require('./index').then(sap => {
   sap.ui.require([
     'sap/ui/model/odata/v2/ODataModel'
-  ], function (ODataModel) {
+], async function (ODataModel) {
     console.log('Creating ODataModel...')
     const model = new ODataModel({
       serviceUrl: 'https://services.odata.org/V2/OData/OData.svc'
     })
-    model.metadataLoaded().then(() => {
-      console.log('Loading products...')
-      model.read('/Products', {
-        success: data => {
-          console.log(data)
-        }
-      })
+    await model.metadataLoaded();
+    console.log('Loading products...')
+    model.read('/Products', {
+      success: data => {
+        data.results.forEach(product => {
+            console.log(product.Name.padEnd(20, ' '), product.Description)
+        })
+      }
     })
   })
 })
