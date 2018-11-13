120f1dfb3743b566e5f8ec113e0af224021d629e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-12 23:27:22 -0500

    StandardJS

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ require('./index').then(sap => {
   ], function (ODataModel) {
     console.log('Creating ODataModel...')
     const model = new ODataModel({
-        serviceUrl: 'https://services.odata.org/V2/OData/OData.svc'
+      serviceUrl: 'https://services.odata.org/V2/OData/OData.svc'
     })
     model.metadataLoaded().then(() => {
       console.log('Loading products...')
