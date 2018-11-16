7e5a6c1f0defe4174b280d0e4cec101fac1d5ab6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-16 15:27:43 -0500

    OData client tests

---
+++ tests/odata-client/tests.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = sap => {
+  sap.ui.require([
+    'sap/ui/model/odata/v2/ODataModel'
+  ], async function (ODataModel) {
+    console.log('Creating ODataModel...')
+    const model = new ODataModel({
+      serviceUrl: 'https://services.odata.org/V2/OData/OData.svc'
+    })
+    await model.metadataLoaded()
+    console.log('Loading products...')
+    model.read('/Products', {
+      success: data => {
+        data.results.forEach(product => {
+          console.log(product.Name.padEnd(20, ' '), product.Description)
+        })
+      }
+    })
+  })
+}
