e5ab10a918272ca637acdc5048015ffe51573924
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-17 09:08:45 -0500

    Verbose option

--- factory.js
+++ factory.js
@@ -1,14 +1,17 @@
 'use strict'
 
+const path = require('path')
 const browserFactory = require('./src/browser')
 
 module.exports = (userSettings = {}) => {
   const {
     baseURL = 'http://node-ui5.server.net/',
-    bootstrapLocation = 'resources/sap-ui-core.js'
+    bootstrapLocation = 'resources/sap-ui-core.js',
+    verbose = false
   } = userSettings
   return browserFactory({
     baseURL,
-    bootstrapLocation
+    bootstrapLocation,
+    verbose
   })
 }
