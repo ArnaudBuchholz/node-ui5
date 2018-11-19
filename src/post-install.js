f72661d6495fc67529e0d6c16a5a43f86127d6e3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-11-18 23:37:19 -0500

    Post install to build ui5 core lib

---
+++ src/post-install.js
@@ -0,0 +1,35 @@
+'use strict'
+
+require('colors')
+const moduleHelper = require('./src/moduleHelper')
+const path = require('path')
+const child_process = require('child_process')
+
+const ui5corePath = moduleHelper.find('@openui5/sap.ui.core')
+const ui5buildPath = moduleHelper.find('@ui5/cli')
+
+const distPath = path.join(ui5corePath, 'dist')
+if (moduleHelper.exists(distPath)) {
+  console.log('@openui5/sap.ui.core/dist folder already exists'.green)
+  process.exit(0)
+}
+
+console.log('Building @openui5/sap.ui.core/dist...'.magenta)
+
+const ui5build = child_process.spawn('node', [
+  path.join(ui5buildPath, 'bin/ui5'),
+  'build',
+  '--a'
+], {
+  cwd: ui5corePath,
+  stdio: 'inherit'
+})
+
+ui5build.on('error', () => {
+  console.error('ui5 build failed.'.red)
+  process.exit(-1)
+})
+ui5build.on('close', () => {
+  console.error('ui5 build done.'.green)
+  process.exit(0)
+})
