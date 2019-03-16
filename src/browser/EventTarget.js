06007d278215f4bd00f5d51bb0daf1f91f1f8fcc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-16 12:40:53 -0400

    Fix the mixin algorithm

--- src/browser/EventTarget.js
+++ src/browser/EventTarget.js
@@ -8,7 +8,13 @@ class EventTarget {
 }
 
 EventTarget.mixin = MixinClass => {
-  Object.assign(MixinClass.prototype, EventTarget.prototype)
+  Object.getOwnPropertyNames(EventTarget.prototype)
+    .filter(member => member !== "constructor")
+    .forEach(member => {
+      Object.defineProperty(MixinClass.prototype, member, {
+        value: EventTarget.prototype[member]
+      })
+    })
 }
 
 module.exports = EventTarget
