3e2c8819594a63a0d1064d3b72dcb2af122cc296
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-17 23:33:42 -0400

    Passing settings

--- src/browser/Document.js
+++ src/browser/Document.js
@@ -1,21 +1,24 @@
 'use strict'
 
+const { $settings } = require('./const')
 const Node = require('./Node')
 
 class Document extends Node {
+  constructor (settings) {
+    super(settings)
+  }
 
   createElement () {
-    return new Node()
+    return new Node(this[$settings])
   }
 
   get documentElement () {
-    return new Node()
+    return new Node(this[$settings])
   }
 
   get scripts () {
     return []
   }
-
 }
 
 module.exports = Document
