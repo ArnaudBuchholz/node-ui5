c4fa8b2f95e854b1c52a6c10a6d67880f2b5e553
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-17 23:33:50 -0400

    Implements EventTarget

--- src/browser/EventTarget.js
+++ src/browser/EventTarget.js
@@ -1,15 +1,32 @@
 'use strict'
 
-class EventTarget {
+const $events = Symbol('events')
 
-  addEventListener () {
+class EventTarget {
+  addEventListener (type, eventHandler) {
+    if (!this[$events]) {
+      this[$events] = {}
+    }
+    if (!this[$events][type]) {
+      this[$events][type] = []
+    }
+    this[$events][type].push(eventHandler)
   }
 
+  dispatchEvent (event) {
+    const type = event.type
+    if (this[`on${type}`]) {
+      this[`on${type}`]()
+    }
+    if (this[$events] && xhr[$events][type]) {
+      xhr[$events][type].forEach(eventHandler => eventHandler(this))
+    }
+  }
 }
 
 EventTarget.mixin = MixinClass => {
   Object.getOwnPropertyNames(EventTarget.prototype)
-    .filter(member => member !== "constructor")
+    .filter(member => member !== 'constructor')
     .forEach(member => {
       Object.defineProperty(MixinClass.prototype, member, {
         value: EventTarget.prototype[member]
