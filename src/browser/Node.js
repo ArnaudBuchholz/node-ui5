2daa15114f63fb34ceed94c92aaa13c6e55701c5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-19 15:55:36 -0400

    Settings are kept at window level Isolates Element parts

--- src/browser/Node.js
+++ src/browser/Node.js
@@ -2,73 +2,69 @@
 
 const EventTarget = require('./EventTarget')
 
-const { $settings, $window } = require('./const')
-const $attributes = Symbol('attributes')
+const {
+  $name,
+  $nodeType,
+  $settings,
+  $window
+} = require('./const')
 const $childNodes = Symbol('childNodes')
-const $nodeType = Symbol('nodeType')
-const $style = Symbol('style')
+const $parent = Symbol('parent')
 
 class Node extends EventTarget {
-  constructor (window, settings, nodeType) {
+  constructor (window, nodeType) {
     super()
     this[$window] = window
-    this[$settings] = settings
     this[$nodeType] = nodeType
-    this[$attributes] = {}
     this[$childNodes] = []
-    this[$style] = {}
   }
 
   appendChild (node) {
+    node[$parent] = this
     this[$childNodes].push(node)
     return node
   }
 
   get baseURI () {
-    return this[$settings].baseURL
+    return this[$window][$settings].baseURL
   }
 
   get childNodes () {
     return this[$childNodes]
   }
 
-  get className () {
-    return this._className || ''
+  _cloneNode () {
+    const clone = new Node(this[$window], this[$nodeType])
+    clone[$name] = this[$name]
+    return clone
   }
 
   cloneNode (deep) {
-    const clone = new Node(this[$window], this[$settings], this[$nodeType])
-    clone[$attributes] = {...this[$attributes]}
+    const clone = this._cloneNode()
     if (deep) {
       this[$childNodes].forEach(child => clone[$childNodes].push(child.cloneNode(true)))
     }
     return clone
   }
 
-  set className (value) {
-    this._className = value
+  compareDocumentPosition (otherNode) {
+    // Check only for disconnected
+    if (!this.parentNode || !otherNode.parentNode) {
+      return Node.DOCUMENT_POSITION_DISCONNECTED
+    }
+    return 0
   }
 
   get firstChild () {
-      const length = this[$childNodes].length
-      if (length) {
-          return this[$childNodes][0]
-      }
-      return null
-  }
-
-  getAttribute (name) {
-      if (name === "className") {
-        return this.className
-      }
-      return this[$attributes][name]
-  }
-
-  getElementsByTagName () {
-      return []
+    const length = this[$childNodes].length
+    if (length) {
+      return this[$childNodes][0]
+    }
+    return null
   }
 
   insertBefore (node, refNode) {
+    node[$parent] = this
     const pos = this[$childNodes].indexOf(refNode)
     this[$childNodes].splice(pos, 0, node)
   }
@@ -76,9 +72,17 @@ class Node extends EventTarget {
   get lastChild () {
     const length = this[$childNodes].length
     if (length) {
-        return this[$childNodes][length - 1]
+      return this[$childNodes][length - 1]
     }
     return null
+  }
+
+  get localName () {
+    return this[$name]
+  }
+
+  get nodeName () {
+    return this[$name]
   }
 
   get nodeType () {
@@ -89,31 +93,29 @@ class Node extends EventTarget {
     return this[$window].document
   }
 
-  querySelector () {
-    return null
-  }
-
-  querySelectorAll () {
-      return []
+  get parentNode () {
+    return this[$parent] || null
   }
 
   removeChild (node) {
     const pos = this[$childNodes].indexOf(node)
     if (pos !== -1) {
-        this[$childNodes].splice(pos, 1)
+      this[$childNodes].splice(pos, 1)
     }
-  }
-
-  setAttribute (name, value) {
-    this[$attributes][name] = value
-  }
-
-  get style () {
-      return this[$style]
   }
 }
 
-const nodeTypes = {
+function defineConstants (constants) {
+  Object.keys(constants).forEach(name => {
+    Object.defineProperty(Node, name, {
+      value: constants[name],
+      writable: false
+    })
+  })
+}
+
+// Node types
+defineConstants({
   ELEMENT_NODE: 1,
   TEXT_NODE: 3,
   CDATA_SECTION_NODE: 4,
@@ -122,13 +124,16 @@ const nodeTypes = {
   DOCUMENT_NODE: 9,
   DOCUMENT_TYPE_NODE: 10,
   DOCUMENT_FRAGMENT_NODE: 11
-}
+})
 
-Object.keys(nodeTypes).forEach(name => {
-  Object.defineProperty(Node, name, {
-    value: nodeTypes[name],
-    writable: false
-  })
+// Document position mask
+defineConstants({
+  DOCUMENT_POSITION_DISCONNECTED: 1,
+  DOCUMENT_POSITION_PRECEDING: 2,
+  DOCUMENT_POSITION_FOLLOWING: 4,
+  DOCUMENT_POSITION_CONTAINS: 8,
+  DOCUMENT_POSITION_CONTAINED_BY: 16,
+  DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC: 32
 })
 
 module.exports = Node
