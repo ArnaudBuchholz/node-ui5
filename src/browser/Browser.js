601ee24fe97543f91e27d9304f95fa161ff6e124
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-20 13:11:24 -0400

    eval moved to window

--- src/browser/Browser.js
+++ src/browser/Browser.js
@@ -10,20 +10,6 @@ class Browser {
     this[$window].location = new URL(settings.baseURL)
   }
 
-  eval (code) {
-    // Create a secure context
-    const window = this[$window]
-    const params = ['window', 'global', 'require']
-    const values = [window]
-    const securedContext = Function.apply(null, params.concat(`with (window) { ${code} }`))
-    try {
-      debugger
-      securedContext.apply(window, values)
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
   get window () {
     return this[$window]
   }
