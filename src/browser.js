bbec5620af8b6b200d63b7941a014760d03b889f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-21 13:17:17 -0400

    Switch between JSDOM & minimal DOM implementation

--- src/browser.js
+++ src/browser.js
@@ -2,15 +2,13 @@
 
 const path = require('path')
 const resources = require('./resources')
-const Browser = require('./browser/Browser')
 
-module.exports = settings => {
-  let factoryResolve
-  let factoryReject
-
+module.exports = async function (settings) {
+  let promiseResolve
+  let promiseReject
   const promise = new Promise((resolve, reject) => {
-    factoryResolve = resolve
-    factoryReject = reject
+    promiseResolve = resolve
+    promiseReject = reject
   })
 
   const resourceroots = Object.keys(settings.resourceroots).reduce((roots, root) => {
@@ -20,82 +18,47 @@ module.exports = settings => {
     'node-ui5': resources.declare(path.join(__dirname, '../lib'))
   })
 
-  const browser = new Browser(settings)
-  browser.window['sap-ui-config'] = {
-    resourceRoots: {
-      '': settings.baseURL + 'resources/', // TODO needs to be adapted depending on settings
-      ...resourceroots
+  let window
+  if (settings.fastButIncompleteSimulation) {
+    window = require('./mindom/factory')(settings)
+  } else {
+    window = require('./jsdom/factory')(settings)
+  }
+
+  // Inject factory hooks
+  window.__factory__ = {
+    resolve: sap => {
+      if (settings.exposeAsGlobals) {
+        global.window = window
+        global.sap = sap
+      }
+      promiseResolve({ window, sap })
+    },
+    reject: reason => {
+      promiseReject(reason)
     }
   }
 
-  Promise.resolve()
-    .then(() => resources.read(settings, settings.bootstrapLocation))
-    .then(bootstrap => {
-      browser.window.eval(bootstrap)
+  // Create the UI5 bootstrap node
+  var ui5Boot = window.document.createElement('script')
+  ui5Boot.id = 'sap-ui-bootstrap'
+  ui5Boot.setAttribute('src', settings.bootstrapLocation)
+  ui5Boot.setAttribute('data-sap-ui-compatVersion', 'edge')
+  ui5Boot.setAttribute('data-sap-ui-frameOptions', 'allow')
+  ui5Boot.setAttribute('data-sap-ui-resourceroots', JSON.stringify(resourceroots))
+  window.document.documentElement.appendChild(ui5Boot)
+
+  // Create the custom bootstrap node
+  var customBoot = window.document.createElement('script')
+  customBoot.textContent = `
+  if (typeof sap !== 'undefined') {
+    sap.ui.getCore().attachInit(function() {
+      window.__factory__.resolve(sap)
     })
-  //
-  // // Creating a simulated browser
-  // const browser = new JSDOM(`
-  //   <script id="sap-ui-bootstrap"
-  //     src="${settings.bootstrapLocation}"
-  //     data-sap-ui-libs=""
-  //     data-sap-ui-theme=""
-  //     data-sap-ui-compatVersion="edge"
-  //     data-sap-ui-frameOptions='allow'
-  //     data-sap-ui-preload=""
-  //     data-sap-ui-resourceroots='${resourceroots}'>
-  //   </script>
-  //   <script>
-  //     'use strict'
-  //     if (typeof sap !== 'undefined') {
-  //       sap.ui.getCore().attachInit(function() {
-  //         window.__factory__.resolve(sap)
-  //       })
-  //     } else {
-  //       window.__factory__.reject(new Error('Invalid bootstrap'))
-  //     }
-  //   </script>
-  // `, {
-  //   url: settings.baseURL,
-  //   referrer: settings.baseURL,
-  //   contentType: 'text/html',
-  //   includeNodeLocations: true,
-  //   storageQuota: 10000000,
-  //   runScripts: 'dangerously',
-  //   resources: new ResourceLoader(settings),
-  //   beforeParse: window => {
-  //     // Inject factory hooks
-  //     window.__factory__ = {
-  //       resolve: sap => {
-  //         if (settings.exposeAsGlobals) {
-  //           global.browser = browser
-  //           global.window = window
-  //           global.sap = sap
-  //         }
-  //         factoryResolve({ browser, window, sap })
-  //       },
-  //       reject: reason => {
-  //         factoryReject(reason)
-  //       }
-  //     }
-  //     // Controlling traces
-  //     window.console = {}
-  //     'dir,error,info,log,warn'.split(',').forEach(name => {
-  //       window.console[name] = (...args) => {
-  //         if (settings.verbose) {
-  //           console[name](...args)
-  //         }
-  //       }
-  //     })
-  //     // Compatibility layer (see https://developer.mozilla.org/en-US/docs/Web/API/PerformanceTiming/fetchStart)
-  //     window.performance.timing = {
-  //       navigationStart: new Date().getTime(),
-  //       fetchStart: new Date().getTime()
-  //     }
-  //     // Wrap XHR
-  //     require('./xhr')(settings, window.XMLHttpRequest)
-  //   }
-  // })
+  } else {
+    window.__factory__.reject(new Error('Invalid bootstrap'))
+  }`
+  window.document.documentElement.appendChild(customBoot)
 
   return promise
 }
