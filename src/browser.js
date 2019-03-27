63a122085b38f3757369e6f936469095f7589710
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-27 16:39:53 -0400

    Tweak detection of theme loading

--- src/browser.js
+++ src/browser.js
@@ -70,9 +70,10 @@ module.exports = async function (settings) {
   }
   window.document.documentElement.appendChild(ui5Boot)
 
-  // Create the custom bootstrap node
+  // Create the custom bootstrap node (& tweak theme loading detection)
   var customBoot = window.document.createElement('script')
   customBoot.textContent = `
+  document.getElementById('sap-ui-theme-sap.ui.core').setAttribute('data-sap-ui-ready', 'true')
   if (typeof sap !== 'undefined') {
     sap.ui.getCore().attachInit(function() {
       window.__factory__.resolve(sap)
