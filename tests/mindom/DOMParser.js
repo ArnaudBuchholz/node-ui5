24985056b2710bd7a8e178185bf7b7a7bfbd583c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-03-27 23:11:02 -0400

    More tests

--- tests/mindom/DOMParser.js
+++ tests/mindom/DOMParser.js
@@ -28,31 +28,32 @@ assert(() => window)
 const parser = new window.DOMParser()
 assert(() => parser)
 
-function parse (string) {
+const HTML = 'text/html'
+const XML = 'application/xml'
+
+function parse (string, type) {
   console.log(string.yellow)
-  const doc = parser.parseFromString(string)
+  const doc = parser.parseFromString(string, type)
   console.log(doc.innerHTML.gray)
+  assert(() => doc.nodeType === Node.DOCUMENT_NODE)
   return doc
 }
 
 const tests = [() => {
-  const doc = parse('<html />')
-  assert(() => doc.nodeType === Node.DOCUMENT_NODE)
+  const doc = parse('<html />', HTML)
   const html = doc.firstChild
   assert(() => html && html.nodeType === Node.ELEMENT_NODE)
   assert(() => html.nodeName === 'html')
   assert(() => html.childNodes.length === 0)
 }, () => {
-  const doc = parse('<html style="border: 1px;"/>')
-  assert(() => doc.nodeType === Node.DOCUMENT_NODE)
+  const doc = parse('<html style="border: 1px;"/>', HTML)
   const html = doc.firstChild
   assert(() => html && html.nodeType === Node.ELEMENT_NODE)
   assert(() => html.nodeName === 'html')
   assert(() => html.childNodes.length === 0)
   assert(() => html.getAttribute('style') === 'border: 1px;')
 }, () => {
-  const doc = parse('<html><head /><body><h1>Hello</h1></body></html>')
-  assert(() => doc.nodeType === Node.DOCUMENT_NODE)
+  const doc = parse('<html><head /><body><h1>Hello</h1></body></html>', HTML)
   const html = doc.firstChild
   assert(() => html && html.nodeType === Node.ELEMENT_NODE)
   assert(() => html.nodeName === 'html')
@@ -67,6 +68,22 @@ const tests = [() => {
   assert(() => h1 && h1.nodeType === Node.ELEMENT_NODE)
   assert(() => h1.nodeName === 'h1')
   assert(() => h1.textContent === 'Hello')
+}, () => {
+  const doc = parse('<!-- This is a comment -->', HTML)
+  const comment = doc.firstChild
+  assert(() => comment && comment.nodeType === Node.COMMENT_NODE)
+  assert(() => comment.nodeValue === 'This is a comment')
+}, () => {
+  const doc = parse(`<edmx:Edmx
+  	Version="1.0"
+  	xmlns:edmx="http://schemas.microsoft.com/ado/2007/06/edmx"
+  	xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"
+  	xmlns:sap="http://www.sap.com/Protocols/SAPData"
+  />`, XML)
+  const edmx = doc.firstChild
+  assert(() => edmx && edmx.nodeType === Node.ELEMENT_NODE)
+  assert(() => edmx.nodeName === 'edmx:Edmx')
+  assert(() => edmx.getAttribute('Version') === '1.0')
 }]
 
 tests.forEach(test => {
