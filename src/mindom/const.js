ef2b3d6df95acf2998e1372d353960f091a99eac
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-01 13:16:29 -0400

    defineConstants in common

--- src/mindom/const.js
+++ src/mindom/const.js
@@ -5,6 +5,15 @@ module.exports = {
   $name: Symbol('name'),
   $nodeType: Symbol('nodeType'),
   $settings: Symbol('settings'),
-  $window: Symbol('window')
+  $window: Symbol('window'),
+
+  defineConstants: (object, constants) => {
+    Object.keys(constants).forEach(name => {
+      Object.defineProperty(object, name, {
+        value: constants[name],
+        writable: false
+      })
+    })
+  }
 
 }
