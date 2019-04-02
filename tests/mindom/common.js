e6feb8464c75672b62bad61b6e77b821e572dde5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-02 15:15:20 -0400

    Log final result

--- tests/mindom/common.js
+++ tests/mindom/common.js
@@ -1,6 +1,17 @@
 'use strict'
+/* global process */
 
 require('colors')
+
+let failedCount = 0
+
+process.on('exit', () => {
+  if (failedCount) {
+    console.error(`${failedCount} assertions failed`.red)
+  } else {
+    console.error(`All assertions succeeded`.green)
+  }
+})
 
 module.exports = {
 
@@ -19,6 +30,7 @@ module.exports = {
     if (condition) {
       console.log('OK'.green, message)
     } else {
+      ++failedCount
       console.error('KO'.red, message)
     }
   }
