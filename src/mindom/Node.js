472389240d0419a786cc690a93a89a7dc42bc868
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-04 08:49:33 -0400

    ATTRIBUTE_NODE required for XPathResult

--- src/mindom/Node.js
+++ src/mindom/Node.js
@@ -183,8 +183,8 @@ class Node extends EventTarget {
 // Node types
 defineConstants(Node, {
   ELEMENT_NODE: 1,
+  ATTRIBUTE_NODE: 2,
   TEXT_NODE: 3,
-  CDATA_SECTION_NODE: 4,
   PROCESSING_INSTRUCTION_NODE: 7,
   COMMENT_NODE: 8,
   DOCUMENT_NODE: 9,
