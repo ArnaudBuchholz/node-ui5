9f6c34da5dc8ea6dac4b72cb839a69c19f40ade0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-05 12:52:36 -0400

    Forward parameters & measure time spent

--- tests/all.js
+++ tests/all.js
@@ -5,7 +5,7 @@ const { spawn } = require('child_process')
 function run (script) {
   console.log(script.cyan)
   return new Promise(resolve => {
-    const child = spawn('node', [path.join(__dirname, `${script}.js`)], {
+    const child = spawn('node', [path.join(__dirname, `${script}.js`)].concat(process.argv.slice(2)), {
       stdio: 'inherit'
     })
     child.on('close', code => {
@@ -15,10 +15,12 @@ function run (script) {
 }
 
 async function all () {
+  const now = new Date()
   await run('default-bootstrap')
   await run('custom-bootstrap')
   await run('invalid-bootstrap')
   await run('myApp')
+  console.log('Total time (ms): ', new Date() - now)
 }
 
 all()
