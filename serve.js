535da183d9b600bd044949899c3ed942114a7570
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-12 15:44:31 -0400

    Implements basic body parser

--- serve.js
+++ serve.js
@@ -17,46 +17,41 @@ module.exports = function ({
     if (verbose) {
       console.log('SERVE'.magenta, `${request.method} ${request.url}`.gray)
     }
-    // Handle static path first
-    // if file not found, forward
-    /*
-            // Express
-            const express = require('express')
-            const app = express()
-            const logger = require('morgan')
-            const bodyParser = require('body-parser')
-
-            app.use(logger('dev'))
-            app.use(bodyParser.text({
-              type: '*-/-*'
-            }))
-*/
-    window.jQuery.ajax({
-      method: request.method,
-      url: request.url,
-      headers: request.headers,
-      data: request.body, // Need to get them
-      complete: jqXHR => {
-        const status = jqXHR.status
-        const responseText = jqXHR.responseText
-        jqXHR.getAllResponseHeaders()
-          .split('\n')
-          .filter(header => header)
-          .forEach(header => {
-            const pos = header.indexOf(':')
-            response.setHeader(header.substr(0, pos).trim(), header.substr(pos + 1).trim())
-          })
-        response.statusCode = status
-        response.end(responseText)
-        let report
-        if (status.toString().startsWith(2)) {
-          report = `${status} ${responseText.length}`.green
-        } else {
-          report = status.toString().red
-        }
-        report += ` ${new Date() - start} ms`.magenta
-        console.log('SERVE'.magenta, `${request.method} ${request.url}`.cyan, report)
+    const bodyChunks = []
+    request.on('data', chunk => bodyChunks.push(chunk.toString()))
+    request.on('end', () => {
+      if (bodyChunks.length) {
+        request.body = bodyChunks.join('')
       }
+      // Handle static path first
+      // if file not found, forward
+      window.jQuery.ajax({
+        method: request.method,
+        url: request.url,
+        headers: request.headers,
+        data: request.body, // Need to get them
+        complete: jqXHR => {
+          const status = jqXHR.status
+          const responseText = jqXHR.responseText
+          jqXHR.getAllResponseHeaders()
+            .split('\n')
+            .filter(header => header)
+            .forEach(header => {
+              const pos = header.indexOf(':')
+              response.setHeader(header.substr(0, pos).trim(), header.substr(pos + 1).trim())
+            })
+          response.statusCode = status
+          response.end(responseText)
+          let report
+          if (status.toString().startsWith(2)) {
+            report = `${status} ${responseText.length}`.green
+          } else {
+            report = status.toString().red
+          }
+          report += ` ${new Date() - start} ms`.magenta
+          console.log('SERVE'.magenta, `${request.method} ${request.url}`.cyan, report)
+        }
+      })
     })
   })
   server.listen(port, hostname, () => {
