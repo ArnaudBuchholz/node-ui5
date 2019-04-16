6dbcf96027331c5420fcf8225450b8da7c266d15
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-15 23:38:48 -0400

    run all tests and ensure they worked

--- tests/all.js
+++ tests/all.js
@@ -1,6 +1,6 @@
 require('colors')
 const path = require('path')
-const { spawn } = require('child_process')
+const { spawn, fork } = require('child_process')
 
 function run (script) {
   console.log(script.cyan)
@@ -9,18 +9,43 @@ function run (script) {
       stdio: 'inherit'
     })
     child.on('close', code => {
-      resolve(code)
+      resolve(code !== 0)
     })
   })
 }
 
+let server
+function startServer () {
+  console.log('Starting server'.cyan)
+  return new Promise(resolve => {
+    server = fork(path.join(__dirname, `server/serve.js`), process.argv.slice(2))
+    server.on('message', resolve)
+  })
+}
+
+function stopServer () {
+  server.kill('SIGINT');
+}
+
 async function all () {
   const now = new Date()
-  await run('default-bootstrap')
-  await run('custom-bootstrap')
-  await run('invalid-bootstrap')
-  await run('myApp')
+  let failed = false
+  failed |= await run('default-bootstrap')
+  failed |= await run('custom-bootstrap')
+  failed |= await run('invalid-bootstrap')
+  failed |= await run('myApp')
+  failed |= await run('mindom/DOMParser')
+  failed |= await run('mindom/evaluate')
+  await startServer()
+  failed |= await run('server/using-http-helper')
+  failed |= await run('server/using-odata-model')
   console.log('Total time (ms): ', new Date() - now)
+  if (failed) {
+    console.error(`At least one test failed`.red)
+  } else {
+    console.error(`All tests succeeded`.green)
+  }
+  stopServer()
 }
 
 all()
