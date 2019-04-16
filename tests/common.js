76b2d919a967db7392680a6f0a1d30a7a79f9129
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-15 23:29:08 -0400

    common is now for all tests

---
+++ tests/common.js
@@ -0,0 +1,39 @@
+'use strict'
+/* global process */
+
+require('colors')
+
+let failedCount = 0
+
+process.on('exit', () => {
+  process.exitCode = failedCount
+  if (failedCount) {
+    console.error(`${failedCount} assertions failed`.red)
+  } else {
+    console.error(`All assertions succeeded`.green)
+  }
+})
+
+module.exports = {
+
+  HTML: 'text/html',
+  XML: 'application/xml',
+
+  assert: function (code) {
+    let message = code.toString().match(/(?:=>|{)([^}]*)\}?/)[1].toString().trim()
+    let condition
+    try {
+      condition = code()
+    } catch (e) {
+      condition = false
+      message += ' ' + e.toString().gray
+    }
+    if (condition) {
+      console.log('OK'.green, message)
+    } else {
+      ++failedCount
+      console.error('KO'.red, message)
+    }
+  }
+
+}
