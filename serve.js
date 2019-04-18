002c6ca2b9376aef6dd6253f26b44fa47c4e5ac3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-17 23:26:55 -0400

    redirect mechanism WIP

--- serve.js
+++ serve.js
@@ -1,60 +1,94 @@
 require('colors')
 
 const EventEmitter = require('events')
+const gpf = require('gpf-js')
 const http = require('http')
 
 process.on('unhandledRejection', error => { // Absorb
   console.log('unhandledRejection'.red, (error.message || error.toString()).gray)
 })
 
+function log (request, response, responseText) {
+  const status = response.statusCode.toString()
+  let report
+  if (status.startsWith(2)) {
+    report = `${status} ${responseText.length}`.green
+  } else {
+    report = status.red
+  }
+  report += ` ${new Date() - request.start} ms`.magenta
+  console.log('SERVE'.magenta, `${request.method} ${request.url}`.cyan, report)
+}
+
+function redirectToUrl (request, url, response) {
+  delete request.headers.host; // May cause issues
+  gpf.http.request({...request, url})
+    .then(httpResponse => {
+      Object.keys(httpResponse.headers).forEach(key => {
+        response.setHeader(key, httpResponse.headers[key])
+      })
+      response.statusCode = httpResponse.status;
+      const responseText = httpResponse.responseText
+      response.end(responseText)
+      log(request, response, responseText)
+    })
+}
+
+function forwardToAjax (window, request, response) {
+  window.jQuery.ajax({
+    method: request.method,
+    url: request.url,
+    headers: request.headers,
+    data: request.data, // Need to get them
+    complete: jqXHR => {
+      jqXHR.getAllResponseHeaders()
+        .split('\n')
+        .filter(header => header)
+        .forEach(header => {
+          const pos = header.indexOf(':')
+          response.setHeader(header.substr(0, pos).trim(), header.substr(pos + 1).trim())
+        })
+      response.statusCode = jqXHR.status
+      const responseText = jqXHR.responseText
+      response.end(responseText)
+      log(request, response, responseText)
+    }
+  })
+}
+
 module.exports = function ({
   window,
-  files = '',
+  redirect = [],
   hostname = '127.0.0.1',
   port = 8080,
   verbose = process.argv.some(param => ['--verbose', '--debug'].includes(param))
 }) {
   const eventEmitter = new EventEmitter()
   const server = http.createServer((request, response) => {
-    const start = new Date()
+    request.start = new Date()
     if (verbose) {
       console.log('SERVE'.magenta, `${request.method} ${request.url}`.gray)
     }
-    const bodyChunks = []
-    request.on('data', chunk => bodyChunks.push(chunk.toString()))
+    const dataChunks = []
+    request.on('data', chunk => dataChunks.push(chunk.toString()))
     request.on('end', () => {
-      if (bodyChunks.length) {
-        request.body = bodyChunks.join('')
+      if (dataChunks.length) {
+        request.data = dataChunks.join('')
       }
-      // Handle static path first
-      // if file not found, forward
-      window.jQuery.ajax({
-        method: request.method,
-        url: request.url,
-        headers: request.headers,
-        data: request.body, // Need to get them
-        complete: jqXHR => {
-          const status = jqXHR.status
-          const responseText = jqXHR.responseText
-          jqXHR.getAllResponseHeaders()
-            .split('\n')
-            .filter(header => header)
-            .forEach(header => {
-              const pos = header.indexOf(':')
-              response.setHeader(header.substr(0, pos).trim(), header.substr(pos + 1).trim())
-            })
-          response.statusCode = status
-          response.end(responseText)
-          let report
-          if (status.toString().startsWith(2)) {
-            report = `${status} ${responseText.length}`.green
-          } else {
-            report = status.toString().red
+      if (redirect.every(pattern => {
+        const match = pattern.match.exec(request.url)
+        if (match) {
+          const url = pattern.url.replace(/\$1/g, match[1])
+          if (verbose) {
+            console.log('SERVE'.magenta, `${request.url} => ${url}`.gray)
+            redirectToUrl(request, url, response)
           }
-          report += ` ${new Date() - start} ms`.magenta
-          console.log('SERVE'.magenta, `${request.method} ${request.url}`.cyan, report)
+          return false
         }
-      })
+        return true
+      })) {
+        forwardToAjax(window, request, response);
+      }
     })
   })
   server.listen(port, hostname, () => {
