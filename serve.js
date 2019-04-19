a2e15111c52f7b939cb1e4ed1a143de6566680a3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-04-18 22:56:35 -0400

    redirect URL

--- serve.js
+++ serve.js
@@ -1,7 +1,6 @@
 require('colors')
 
 const EventEmitter = require('events')
-const gpf = require('gpf-js')
 const http = require('http')
 const https = require('https')
 
@@ -9,11 +8,11 @@ process.on('unhandledRejection', error => { // Absorb
   console.log('unhandledRejection'.red, (error.message || error.toString()).gray)
 })
 
-function log (request, response, responseText) {
+function log (request, response, responseLength) {
   const status = response.statusCode.toString()
   let report
   if (status.startsWith(2)) {
-    report = `${status} ${responseText.length}`.green
+    report = `${status} ${responseLength}`.green
   } else {
     report = status.red
   }
@@ -31,48 +30,57 @@ function getUrlHandler (url) {
 function redirectToUrl (request, url, response) {
   const handler = getUrlHandler(url)
   const {
-    method
+    method,
     headers
   } = request
   delete headers.host
   const redirectedRequest = handler.request(url, { method, headers }, redirectedResponse => {
-
+    Object.keys(redirectedResponse.headers).forEach(key => {
+      response.setHeader(key, redirectedResponse.headers[key])
+    })
+    response.statusCode = redirectedResponse.statusCode
+    let responseLength = 0
+    redirectedResponse.on('data', chunk => {
+      response.write(chunk)
+      responseLength += chunk.length
+    })
+    redirectedResponse.on('end', () => {
+      response.end()
+      log(request, response, responseLength)
+    })
   })
-  if (request.data) {
-      redirectedRequest.write(request.data)
-  }
-  redirectedRequest.end()
-  // gpf.http.request({...request, url})
-  //   .then(httpResponse => {
-  //     Object.keys(httpResponse.headers).forEach(key => {
-  //       response.setHeader(key, httpResponse.headers[key])
-  //     })
-  //     response.statusCode = httpResponse.status;
-  //     const responseText = httpResponse.responseText
-  //     response.end(responseText)
-  //     log(request, response, responseText)
-  //   })
+  request.on('data', chunk => redirectedRequest.write(chunk))
+  request.on('end', () => {
+    redirectedRequest.end()
+  })
 }
 
 function forwardToAjax (window, request, response) {
-  window.jQuery.ajax({
-    method: request.method,
-    url: request.url,
-    headers: request.headers,
-    data: request.data, // Need to get them
-    complete: jqXHR => {
-      jqXHR.getAllResponseHeaders()
-        .split('\n')
-        .filter(header => header)
-        .forEach(header => {
-          const pos = header.indexOf(':')
-          response.setHeader(header.substr(0, pos).trim(), header.substr(pos + 1).trim())
-        })
-      response.statusCode = jqXHR.status
-      const responseText = jqXHR.responseText
-      response.end(responseText)
-      log(request, response, responseText)
+  const dataChunks = []
+  request.on('data', chunk => dataChunks.push(chunk.toString()))
+  request.on('end', () => {
+    if (dataChunks.length) {
+      request.data = dataChunks.join('')
     }
+    window.jQuery.ajax({
+      method: request.method,
+      url: request.url,
+      headers: request.headers,
+      data: request.data, // Need to get them
+      complete: jqXHR => {
+        jqXHR.getAllResponseHeaders()
+          .split('\n')
+          .filter(header => header)
+          .forEach(header => {
+            const pos = header.indexOf(':')
+            response.setHeader(header.substr(0, pos).trim(), header.substr(pos + 1).trim())
+          })
+        response.statusCode = jqXHR.status
+        const responseText = jqXHR.responseText
+        response.end(responseText)
+        log(request, response, responseText.length)
+      }
+    })
   })
 }
 
@@ -89,27 +97,23 @@ module.exports = function ({
     if (verbose) {
       console.log('SERVE'.magenta, `${request.method} ${request.url}`.gray)
     }
-    const dataChunks = []
-    request.on('data', chunk => dataChunks.push(chunk.toString()))
-    request.on('end', () => {
-      if (dataChunks.length) {
-        request.data = dataChunks.join('')
-      }
-      if (redirect.every(pattern => {
-        const match = pattern.match.exec(request.url)
-        if (match) {
-          const url = pattern.url.replace(/\$1/g, match[1])
-          if (verbose) {
-            console.log('SERVE'.magenta, `${request.url} => ${url}`.gray)
-            redirectToUrl(request, url, response)
-          }
-          return false
+    if (redirect.every(pattern => {
+      const match = pattern.match.exec(request.url)
+      if (match) {
+        let url = pattern.url
+        for (let capturingGroupIndex = match.length; capturingGroupIndex > 0; --capturingGroupIndex) {
+          url = url.replace(new RegExp(`\\$${capturingGroupIndex}`, 'g'), match[capturingGroupIndex])
         }
-        return true
-      })) {
-        forwardToAjax(window, request, response);
+        if (verbose) {
+          console.log('SERVE'.magenta, `${request.url} => ${url}`.gray)
+          redirectToUrl(request, url, response)
+        }
+        return false
       }
-    })
+      return true
+    })) {
+      forwardToAjax(window, request, response);
+    }
   })
   server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`.yellow)
