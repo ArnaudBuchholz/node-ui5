316acf0f7613d114908ee0de27c090ad619b0070
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-02 14:34:57 -0400

    Test server with chrome

--- tests/all.js
+++ tests/all.js
@@ -1,6 +1,7 @@
 require('colors')
 const path = require('path')
 const { spawn, fork } = require('child_process')
+const HeadlessChrome = require('simple-headless-chrome')
 
 function run (script) {
   console.log(script.cyan)
@@ -15,12 +16,37 @@ function run (script) {
 }
 
 let server
+let onChromeMessage
+
 function startServer () {
   console.log('Starting server'.cyan)
   return new Promise(resolve => {
     server = fork(path.join(__dirname, `server/serve.js`), process.argv.slice(2))
-    server.on('message', resolve)
+    server.on('message', message => {
+      console.log('server message'.yellow, message.gray)
+      if (message === 'ready') {
+        resolve()
+      } else {
+        onChromeMessage(message === 'KO')
+      }
+    })
   })
+}
+
+async function testWithChrome () {
+  const browser = new HeadlessChrome({ headless: true })
+  const promise = new Promise(resolve => { onChromeMessage = resolve })
+  await browser.init()
+  const mainTab = await browser.newTab({ privateTab: false })
+  await mainTab.goTo('http://localhost:8080')
+    .then(function () {
+      console.log('Chrome started...'.gray)
+    })
+  return promise
+    .then(result => browser.close().then(() => {
+      console.log('Chrome stopped.'.gray)
+      return result
+    }))
 }
 
 function stopServer () {
@@ -39,13 +65,16 @@ async function all () {
   await startServer()
   failed |= await run('server/using-http-helper')
   failed |= await run('server/using-odata-model')
+  failed |= await testWithChrome()
   console.log('Total time (ms): ', new Date() - now)
+  stopServer()
   if (failed) {
     console.error(`At least one test failed`.red)
+    process.exit(-1)
   } else {
     console.error(`All tests succeeded`.green)
+    process.exit(0)
   }
-  stopServer()
 }
 
 all()
