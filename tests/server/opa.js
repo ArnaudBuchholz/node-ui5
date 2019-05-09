2ec0061094c102416ecf7f98389ff3beddcfca66
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-09 14:00:25 -0400

    Small refactoring

--- tests/server/opa.js
+++ tests/server/opa.js
@@ -8,51 +8,30 @@ sap.ui.require([
 ], function (Opa5, opaTest, Press, Properties) {
   'use strict'
 
-  QUnit.module('Simple server action')
+  QUnit.module('OPA end to end example')
 
   Opa5.extendConfig({
     autoWait: true
   })
 
+  function clickButton (oOpa, mProperties, sLabel) {
+      oOpa.waitFor({
+        controlType: 'sap.m.Button',
+        matchers: [new Properties(mProperties)],
+        actions: new Press(),
+        success: function () {
+          Opa5.assert.ok(true, sLabel)
+        }
+      })
+  }
+
   opaTest('Should get a response', function (Given, When, Then) {
     // Act
     Given.iStartMyAppInAFrame('/proxy/https/openui5.hana.ondemand.com/#/entity/sap.uxap.ObjectPageLayout/sample/sap.uxap.sample.ObjectPageOnJSON')
 
-    // Use full screen
-    When.waitFor({
-      controlType: 'sap.m.Button',
-      matchers: [new Properties({
-        icon: 'sap-icon://full-screen'
-      })],
-      actions: new Press(),
-      success: function () {
-        Opa5.assert.ok(true, 'Full screen')
-      }
-    })
-
-    // Click show code
-    When.waitFor({
-      controlType: 'sap.m.Button',
-      matchers: [new Properties({
-        icon: 'sap-icon://syntax'
-      })],
-      actions: new Press(),
-      success: function () {
-        Opa5.assert.ok(true, 'Show code')
-      }
-    })
-
-    // Click API reference
-    When.waitFor({
-      controlType: 'sap.m.Button',
-      matchers: [new Properties({
-        text: 'API Reference'
-      })],
-      actions: new Press(),
-      success: function () {
-        Opa5.assert.ok(true, 'API reference')
-      }
-    })
+    clickButton(When, { icon: 'sap-icon://full-screen' }, 'Full screen')
+    clickButton(When, { icon: 'sap-icon://syntax' }, 'Show code')
+    clickButton(When, { text: 'API Reference' }, 'API reference')
 
     // Then.iTeardownMyAppFrame()
   })
