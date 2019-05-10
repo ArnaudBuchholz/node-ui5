3b47fd023330e6780c4bb962beb3d655c32c1ea5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-05-09 21:53:58 -0400

    Serve with shared mappings

--- tests/server/serve.js
+++ tests/server/serve.js
@@ -1,5 +1,7 @@
+'use strict'
+
 /* global process */
-const path = require('path')
+
 require('../../factory')({
   resourceroots: {
     myApp: __dirname
@@ -11,58 +13,7 @@ require('../../factory')({
     require('../../serve')({
       window,
       port: 8080,
-      mappings: [{
-        // http/https proxy
-        match: /^\/proxy\/(https?)\/(.*)/,
-        url: '$1://$2'
-      }, {
-        // ui5 resource access
-        match: /\/resources\/(.*)/,
-        ui5resources: '$1'
-      }, {
-        // mock server mapping
-        match: /^(\/odata\/.*)/,
-        mock: '$1'
-      }, {
-        // mock server mapping (with a different base URL)
-        match: /^\/api\/(.*)/,
-        mock: '/odata/TODO_SRV/$1'
-      }, {
-        // custom echo handler
-        match: /^\/echo\/(.*)$/,
-        custom: (request, response, path) => {
-          response.writeHead(200, {
-            'Content-Type': 'text/plain',
-            'Content-Length': path.length
-          })
-          response.end(path)
-        }
-      }, {
-        // custom test handler to signal end of browser tests
-        match: /^\/chrome\/(.*)$/,
-        custom: (request, response, status) => {
-          if (process.send) {
-            process.send(status)
-          }
-          response.writeHead(200, {
-            'Content-Type': 'text/plain',
-            'Content-Length': 0
-          })
-          response.end()
-        }
-      }, {
-        // gpf.js
-        match: /^\/gpf\.js$/,
-        file: path.join(__dirname, '../../node_modules/gpf-js/build/gpf.js')
-      }, {
-        // default access to index.html
-        match: /^\/$/,
-        file: path.join(__dirname, 'index.html')
-      }, {
-        // mapping to file access
-        match: /(.*)/,
-        file: path.join(__dirname, '$1')
-      }]
+      mappings: require('./mappings')
     }).on('ready', () => {
       if (process.send) {
         process.send('ready')
