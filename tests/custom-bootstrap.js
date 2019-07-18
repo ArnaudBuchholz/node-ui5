2b0638dcb41dad35937c417388e08e3e54bc6873
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-07-17 20:51:42 -0400

    bootstrapCache

--- tests/custom-bootstrap.js
+++ tests/custom-bootstrap.js
@@ -1,3 +1,5 @@
+const { join } = require('path')
 require('./odata-client')(require('../factory')({
-  bootstrapLocation: 'https://openui5.hana.ondemand.com/resources/sap-ui-core.js'
+  bootstrapLocation: 'https://openui5.hana.ondemand.com/resources/sap-ui-core.js',
+  bootstrapCache: join(__dirname, 'cache')
 }))
