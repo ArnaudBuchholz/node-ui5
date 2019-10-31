d2e358b1bfa6a1582c17dbaec53f924c32d91dd0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-10-31 15:31:41 -0400

    All browser console methods go through traces

--- src/Console.js
+++ src/Console.js
@@ -1,35 +1,29 @@
 'use strict'
 
 require('colors')
+const Traces = require('./Traces')
+
+const mappings = {
+  log: Traces.INFO,
+  debug: Traces.DEBUG,
+  info: Traces.INFO,
+  warn: Traces.WARNING,
+  error: Traces.ERROR
+}
 
 class Console {
   constructor (settings) {
     this._settings = settings
-    'log,debug,info,warn,error'
-      .split(',')
-      .forEach(method => {
-        Object.defineProperty(this, method, {
-          value: Console.prototype._output.bind(this, method),
-          enumerable: true
-        })
+    Object.keys(mappings).forEach(method => {
+      Object.defineProperty(this, method, {
+        value: Console.prototype._output.bind(this, mappings[method]),
+        enumerable: true
       })
+    })
   }
 
-  _output (method, ...params) {
-    if (this._settings.traces.console()) {
-      console.log.apply(console, params
-        .map(param => {
-          if (typeof param === 'string') {
-            return param
-          }
-          if (typeof param === 'object') {
-            return JSON.stringify(param)
-          }
-          return param.toString()
-        })
-        .map(text => text.gray)
-      )
-    }
+  _output (level, ...params) {
+    this._settings.traces.console(level, ...params);
   }
 }
 
