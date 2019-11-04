d80baf484531d8f5f2475cf7aa909104ea70cd6a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-04 15:01:41 -0500

    Use bootstrap tester

--- tests/mocha/default-bootstrap.test.js
+++ tests/mocha/default-bootstrap.test.js
@@ -1,18 +1,5 @@
 'use strict'
 
-const console = require('./null')
-const assert = require('./assert')
-
-describe('default bootstrap', () => {
-
-  let _sap
-
-  before(async function () {
-    this.timeout(10000)
-    const { sap } = await require('../../index')
-    _sap = sap
-  })
-
-  it('enables odata testing', () => require('../odata-client')({ sap: _sap, console, assert }))
-
+require('./bootstrap')({
+  title: 'default bootstrap'
 })
