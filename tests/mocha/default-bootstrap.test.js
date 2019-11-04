23debadb9cfc81acd2d6031960aea25571ff232e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-04 13:37:23 -0500

    mocha test for default bootstrap

--- tests/mocha/default-bootstrap.test.js
+++ tests/mocha/default-bootstrap.test.js
@@ -1,13 +1,18 @@
 'use strict'
 
+const console = require('./null')
+const assert = require('./assert')
+
 describe('default bootstrap', () => {
 
   let _sap
 
-  before(() => require('../../index').then(({ sap }) => {
+  before(async function () {
+    this.timeout(10000)
+    const { sap } = await require('../../index')
     _sap = sap
-  }))
+  })
 
-  it('enables odata testing', () => require('../odata-client')(Promise.resolve({ sap: _sap})))
+  it('enables odata testing', () => require('../odata-client')({ sap: _sap, console, assert }))
 
 })
