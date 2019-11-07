bd65edadd46ac3e3cfa41cd59d1e88984c2afead
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-07 13:24:00 -0500

    debug bootstrap

--- tests/debug-bootstrap.js
+++ tests/debug-bootstrap.js
@@ -1,3 +1,9 @@
-require('./odata-client')(require('../factory')({
+'use strict'
+
+const { assert } = require('./common')
+
+require('../factory')({
   bootstrapLocation: 'openui5@latest/debug'
-}))
+}).then(({ sap }) => {
+  require('./odata-client')({ sap, assert })
+})
